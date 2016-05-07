d64e60b973967a55a14f8a2759a303ce0d118053
matt-preston <matt.r.preston@gmail.com>
2016-05-07 21:41:58 +0100

    Implemented more game logic

---
+++ Assets/Scripts/Atom.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Atom : MonoBehaviour {
+
+	public Sprite atom0;
+	public Sprite atom1;
+	public Sprite atom2;
+	public Sprite atom3;
+	public Sprite atom4;
+
+	public int row;
+	public int column;
+
+	public int count = 0;
+
+    private GameManager.Position position;
+
+	// Use this for initialization
+	void Start () {
+		position = GameManager.instance.RegisterCell (this, row, column);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		GameManager.instance.Select (this);
+	}
+
+	public void Increment() {
+		count++;
+
+        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+
+		switch (count) {
+            case 1:
+                spriteRenderer.sprite = atom1;
+                Debug.Log ("Updating to 1");
+                break;
+            case 2:
+                spriteRenderer.sprite = atom2;
+                Debug.Log ("Updating to 2");
+                break;
+            case 3:
+                spriteRenderer.sprite = atom3;
+                Debug.Log ("Updating to 3");
+                break;
+            default:
+                spriteRenderer.sprite = atom4;
+                Debug.Log ("Updating to 4");
+                break;
+        }
+    }
+
+    public bool OverCapacity() {
+        switch (position) {
+        case GameManager.Position.TOP_LEFT:
+        case GameManager.Position.TOP_RIGHT:
+        case GameManager.Position.BOTTOM_LEFT:
+        case GameManager.Position.BOTTOM_RIGHT:
+            return count > 1;
+        case GameManager.Position.TOP:
+        case GameManager.Position.RIGHT:
+        case GameManager.Position.BOTTOM:
+        case GameManager.Position.LEFT:
+            return count > 2;
+        default:
+            return count > 3;
+        }    
+    }
+
+    public void Explode() {
+        count = 0;
+        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+        spriteRenderer.sprite = atom0;
+        Debug.Log ("Exploded");
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
-	public static GameManager instance;
+    public enum Position { TOP, TOP_LEFT, LEFT, BOTTOM_LEFT, BOTTOM, BOTTOM_RIGHT, RIGHT, TOP_RIGHT, NORMAL}
 
-	public GameObject[,] cells;
+    public static GameManager instance;
+
+    public float speed = 0.2f;
+
+	public Atom[,] atoms;
+    private Stack<Atom> overCapacityAtoms = new Stack<Atom>();
+
+    private float timeToProcessAtoms = -1;
 
 	void Awake() {
 		if (!instance) {
 			instance = gameObject.GetComponent<GameManager> ();
-			cells = new GameObject[8, 8];
+			atoms = new Atom[8, 8];
 		}
 	}
 
@@ -20,16 +28,67 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (timeToProcessAtoms > 0 && Time.time > timeToProcessAtoms) {
+            timeToProcessAtoms = -1;
+            ProcessOverCapacityAtoms ();
+        }
 	}
 
-	public void RegisterCell(GameObject cell, int row, int column) {
+	public Position RegisterCell(Atom atom, int row, int column) {
 		Debug.Log ("Registering cell: " + row + "x" + column);
 
-		if (cells [row, column]) {
+		if (atoms [row, column]) {
 			Debug.LogError ("Duplicate cell at: " + row + "x" + column);
 		}
 
-		cells [row, column] = cell;
+		atoms [row, column] = atom;
+
+        return Position.NORMAL;
 	}
+
+	public void Select(Atom atom) {
+		Debug.Log ("Selected Atom: " + atom);
+
+		// Is valid selection?
+        // Start Move
+
+		atom.Increment ();
+
+        if (atom.OverCapacity ()) {
+            overCapacityAtoms.Push(atom);
+            timeToProcessAtoms = Time.time + speed; 
+        }
+
+        // End Move
+	}
+
+    private void ProcessOverCapacityAtoms() {
+        if (overCapacityAtoms.Count > 0) {
+            Atom next = overCapacityAtoms.Pop ();
+            next.Explode ();
+
+            foreach (Atom neighbour in Neighbours(next)) {
+                neighbour.Increment ();
+                if (neighbour.OverCapacity ()) {
+                    overCapacityAtoms.Push(neighbour);
+                }
+            }
+        }
+
+        if (overCapacityAtoms.Count > 0) {
+            timeToProcessAtoms = Time.time + speed;
+        } else {
+            // End Move
+        }
+    }
+
+    private List<Atom> Neighbours(Atom atom) {
+        // TODO
+        List<Atom> result = new List<Atom> ();
+        result.Add (atoms [atom.row, atom.column - 1]);
+        result.Add (atoms [atom.row, atom.column + 1]);
+        result.Add (atoms [atom.row - 1, atom.column]);
+        result.Add (atoms [atom.row + 1, atom.column]);
+        return result;
+    }
 }
